c62603de7b35f57298cceaa7209d132fbcc26dc7
madebyseven <drownworb@gmail.com>
2026-05-19 17:12:46 +0800

    project data testing

---
+++ js/projectData.js
@@ -0,0 +1,208 @@
+const projects = [
+  {
+    name: "Flying Forward Learning",
+    images: [
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a543b81fb7b916374efbcf_ffl_cover.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5426e8766ded4ad4ab77b_ffl_60.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a543b8c23e20545884cd1c_ffl_59.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5426eaceed181fd84e00f_ffl_58.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5426e87df4a73b8f64670_ffl_57.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5426d704175852f81376a_ffl_55.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a543b8569e51eecf351a9b_ffl_54.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5426ef770ec5d47793618_ffl_53.webp",
+        aspect: "landscape",
+      },
+    ],
+  },
+  {
+    name: "GrowInsight",
+    images: [
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54749f5ccdf3066babf9c_gi_cover.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5474a1fb7b916374fb4f3_gi_1.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5474ad5229c3874a441fd_gi_2.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5474a5876bbcdfea8d4ab_gi_3.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a547490adc26811efc466f_gi_4.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5474a7910d45403cd9887_gi_5.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5474a3d1fadeca94d3d88_gi_6.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a5474a4f917ef6654ce0ca_gi_7.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54749c03a6605e3c46ca8_gi_8.webp",
+        aspect: "landscape",
+      },
+    ],
+  },
+  {
+    name: "LightYears",
+    images: [
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a548d6b67251cf14707a8f_ly_cover%202.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a548d5128c91f102f9d502_ly_23-8.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a548d64598bf85800a60f5_ly_24-8.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a548d6851781df0147df4c_ly_25-8.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a548d69d544768dcf815a6_ly_26-8.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a548d6ecc824473014bc88_ly_27-8.webp",
+        aspect: "landscape",
+      },
+    ],
+  },
+  {
+    name: "KOOLLAB",
+    images: [
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54deee958af5565bed05e_koollab_cover.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54deeb8997cc97e7e6ea4_koollab_13.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54dee4df61a896f188ba3_koollab_14.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54dee8167f7b4951d535a_koollab_28.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54deece33dc6e893af3a6_koollab_39.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54dee31f8432a5a4e8849_koollab_40.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54dee8cdf4cd2188759d5_koollab_77.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54deeea31d88d001f75f2_koollab_87.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54deeda743dfaba9207bf_koollab_88.webp",
+        aspect: "landscape",
+      },
+    ],
+  },
+  {
+    name: "CLAD",
+    images: [
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54e6d3f0affd4777367ac_clad_bus_stop_billboard.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54e6ef89fed9c746e675b_clad_business_card_letterhead.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54e6ec7da4d3b367a22c6_clad_letter_envelope.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54e6d95082dd9f4fe8933_clad_photo_application.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54e6ea9efa2d570e25d3e_clad_tote_bag.webp",
+        aspect: "landscape",
+      },
+    ],
+  },
+  {
+    name: "Alpha Prime",
+    images: [
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54ec6c9c85041b0728bcb_ap_APCI_cover.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54ec55eb103bdebc6bf86_ap_app_icon.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54ec672a52d2a435e6f0f_ap_binder.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54ec655dc91c59946e830_ap_eco_bag.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54ec6be49a30b7287388b_ap_signage.webp",
+        aspect: "landscape",
+      },
+      {
+        src: "https://cdn.prod.website-files.com/699f07d5fb97f612eb588995/69a54ec602ed8f616a5552ae_ap_stationery.webp",
+        aspect: "landscape",
+      },
+    ],
+  },
+  {
+    name: "Index",
+    isIndex: true,
+  },
+];
